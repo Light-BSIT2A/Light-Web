bd3c2063b6ee6a301eb557bcfb936d95232cdda3
Murata Ugetsu <92856337+ugetsu-light@users.noreply.github.com>
2022-09-11 15:45:58 +0800

    fucntions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-
+import Employee from './components/Employee';
 function App() {
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello!</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      {showEmployees?
+        <>
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+        </>
+        :
+        <p>you cannot see the employees</p>
+      }
     </div>
   );
 }
 
 export default App;
-//comment
