e203d70eb83495472cc4a8ecf103eba3651dd4e2
Murata Ugetsu <92856337+ugetsu-light@users.noreply.github.com>
2022-09-11 17:19:20 +0800

    basic styling using tailwind classes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
 function App() {
@@ -11,9 +11,14 @@ function App() {
         <input type='text' onChange={(e)=>{
           setRole(e.target.value)
         }}/>
-          <Employee name="Kai" role="student"/>
-          <Employee name="Sky" role={role}/>
-          <Employee name="Joe"/>
+          <div className="flex flex-wrap justify-center">
+            <Employee name="Kai" role="student" img="https://scontent.fmnl3-1.fna.fbcdn.net/v/t39.30808-6/299198461_763878828256886_3258026637235361374_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF4Ce5lT4BJmfsSpGjQr0C6D5Ea3vT2Mk8PkRre9PYyT3rcpRq03I9OxIucoBB4TqaXYihD4iYhIKoZ4Rif5Q0N&_nc_ohc=LYw9oqnKEpcAX81rltQ&_nc_ht=scontent.fmnl3-1.fna&oh=00_AT-lQfC_bFoPCULXB6BH3Tcp1RCABnc4Jt0vJbYBohQWAQ&oe=6321EB5F"/>
+            <Employee name="Sky" role={role} img="https://scontent.fmnl3-1.fna.fbcdn.net/v/t39.30808-6/299198461_763878828256886_3258026637235361374_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF4Ce5lT4BJmfsSpGjQr0C6D5Ea3vT2Mk8PkRre9PYyT3rcpRq03I9OxIucoBB4TqaXYihD4iYhIKoZ4Rif5Q0N&_nc_ohc=LYw9oqnKEpcAX81rltQ&_nc_ht=scontent.fmnl3-1.fna&oh=00_AT-lQfC_bFoPCULXB6BH3Tcp1RCABnc4Jt0vJbYBohQWAQ&oe=6321EB5F"/>
+            <Employee name="Joe" img="https://scontent.fmnl3-1.fna.fbcdn.net/v/t39.30808-6/299198461_763878828256886_3258026637235361374_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF4Ce5lT4BJmfsSpGjQr0C6D5Ea3vT2Mk8PkRre9PYyT3rcpRq03I9OxIucoBB4TqaXYihD4iYhIKoZ4Rif5Q0N&_nc_ohc=LYw9oqnKEpcAX81rltQ&_nc_ht=scontent.fmnl3-1.fna&oh=00_AT-lQfC_bFoPCULXB6BH3Tcp1RCABnc4Jt0vJbYBohQWAQ&oe=6321EB5F"/>
+            <Employee name="Vandolf" img="https://scontent.fmnl3-1.fna.fbcdn.net/v/t39.30808-6/299198461_763878828256886_3258026637235361374_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF4Ce5lT4BJmfsSpGjQr0C6D5Ea3vT2Mk8PkRre9PYyT3rcpRq03I9OxIucoBB4TqaXYihD4iYhIKoZ4Rif5Q0N&_nc_ohc=LYw9oqnKEpcAX81rltQ&_nc_ht=scontent.fmnl3-1.fna&oh=00_AT-lQfC_bFoPCULXB6BH3Tcp1RCABnc4Jt0vJbYBohQWAQ&oe=6321EB5F"/>
+            <Employee name="Ivan" img="https://scontent.fmnl3-1.fna.fbcdn.net/v/t39.30808-6/299198461_763878828256886_3258026637235361374_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF4Ce5lT4BJmfsSpGjQr0C6D5Ea3vT2Mk8PkRre9PYyT3rcpRq03I9OxIucoBB4TqaXYihD4iYhIKoZ4Rif5Q0N&_nc_ohc=LYw9oqnKEpcAX81rltQ&_nc_ht=scontent.fmnl3-1.fna&oh=00_AT-lQfC_bFoPCULXB6BH3Tcp1RCABnc4Jt0vJbYBohQWAQ&oe=6321EB5F"/>
+            <Employee name="Cheryl" img="https://scontent.fmnl3-1.fna.fbcdn.net/v/t39.30808-6/299198461_763878828256886_3258026637235361374_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF4Ce5lT4BJmfsSpGjQr0C6D5Ea3vT2Mk8PkRre9PYyT3rcpRq03I9OxIucoBB4TqaXYihD4iYhIKoZ4Rif5Q0N&_nc_ohc=LYw9oqnKEpcAX81rltQ&_nc_ht=scontent.fmnl3-1.fna&oh=00_AT-lQfC_bFoPCULXB6BH3Tcp1RCABnc4Jt0vJbYBohQWAQ&oe=6321EB5F"/>
+          </div>
         </>
         :
         <p>you cannot see the employees</p>
