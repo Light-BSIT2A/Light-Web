249b397d1f1d0dd427567dd679b65dfc17ad9577
Murata Ugetsu <92856337+ugetsu-light@users.noreply.github.com>
2022-09-11 18:55:55 +0800

    change modal contents

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -10,7 +10,7 @@ function EditEmployee() {
 
   return (
     <>
-        <button onClick={handleShow} class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+        <button onClick={handleShow} className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
             Update
         </button>
         <Modal
@@ -20,17 +20,47 @@ function EditEmployee() {
             keyboard={false}
         >
             <Modal.Header closeButton>
-                <Modal.Title>Modal title</Modal.Title>
+                <Modal.Title>Update Employee</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                I will not close if you click outside me. Don't even try to press
-                escape key.
+                <form id="editModal" className="w-full max-w-sm">
+                    <div className="md:flex md:items-center mb-6">
+                        <div className="md:w-1/3">
+                        <label
+                            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                            for="name">
+                            Full Name
+                        </label>
+                        </div>
+                        <div className="md:w-2/3">
+                        <input
+                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                            id="name"
+                            type="text"
+                            value="Voe Jandolf"/>
+                        </div>
+                    </div>
+                    <div className="md:flex md:items-center mb-6">
+                        <div className="md:w-1/3">
+                        <label
+                            className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                            for="role">
+                            Role
+                        </label>
+                        </div>
+                        <div className="md:w-2/3">
+                        <input
+                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                            id="role"
+                            type="text"
+                            value="Q.A."/>
+                        </div>
+                    </div>
+                </form>
             </Modal.Body>
             <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-                Close
-            </Button>
-            <Button variant="primary">Understood</Button>
+            <button  onClick={handleClose} className="bg-gray-100 hover:bg-gray-200 text-black py-2 px-4 rounded focus:outline-none focus:shadow-outline">Close</button>
+            <button form="editModal" className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Update</button>
             </Modal.Footer>
         </Modal>
     </>
