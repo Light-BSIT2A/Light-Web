939ea6874937574d0f25bda148b64d6bce4b9816
Murata Ugetsu <92856337+ugetsu-light@users.noreply.github.com>
2022-09-16 22:27:13 +0800

    more errors

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -5,22 +5,46 @@ import PageNotFound from '../components/PageNotFound';
 export default function Definition(){
     const [word, setWord] = useState();
     const [notFound, setNotFound] = useState(false);
+    const [error, setError] = useState(false);
     let {search} = useParams();
     const navigate = useNavigate();
     useEffect(() => {
-        fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+search)
+        //const url = 'https://httpstat.us/501'
+        const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'+search
+        fetch(url)
             .then((response) => {
                 if (response.status === 404){
                     setNotFound(true)
+                } else if (response.status === 401){
+                    navigate('/login')
+                } else if (response.status === 500){
+                    //setServerError(true)
+                }
+
+                if (!response.ok){
+                    setError(true);
+
+                    throw new Error('Something went wrong');
                 }
                 return response.json();
             })
-            .then((data) => {setWord(data[0].meanings);});
+            .then((data) => {setWord(data[0].meanings);})
+            .catch((e) => {
+                console.log(e.message);
+            });
     }, []);
     if (notFound){
         return (
         <>
             <PageNotFound/>
+            <Link to="/dictionary">Search another.</Link>
+        </>
+        );
+    }
+    if (error){
+        return (
+        <>
+            <p>Something went wrong.</p>
             <Link to="/dictionary">Search another.</Link>
         </>
         );
