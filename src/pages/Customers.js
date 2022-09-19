a8bf1d931487023a89dd573a830400100f27b9dc
Murata Ugetsu <92856337+ugetsu-light@users.noreply.github.com>
2022-09-19 20:49:25 +0800

    POST

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
+import AddCustomer from '../components/AddCustomer';
 import { baseUrl } from '../shared';
 export default function Customers(){
     const [customers, setCustomers] = useState();
@@ -9,6 +10,32 @@ export default function Customers(){
         .then((response) => response.json())
         .then((data) => setCustomers(data.customers))
     }, []);
+
+    function newCustomer(name, industry){
+        const data = {name: name, industry: industry};
+        const url = baseUrl + 'api/customers/';
+        fetch(url, {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then((response) => {
+            if(!response.ok){
+                throw new Error('Something went wrong');
+            }
+            return response.json();
+        })
+        .then((data)=>{
+            //assume the add was succesful
+            //hide modal
+            //update list
+        })
+        .catch((e)=>{
+            console.log(e);
+        });
+    }
     return (
         <>
             <h1>Customers: </h1>
@@ -19,6 +46,7 @@ export default function Customers(){
                 :
                 null
             }
+            <AddCustomer newCustomer={newCustomer}/>
         </>
     )
 }
