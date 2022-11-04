8b13c83e29b8dd9d1d3b669a3fd76d84356d1b11
Murata Ugetsu <92856337+ugetsu-light@users.noreply.github.com>
2022-11-04 23:07:08 +0800

    fixed: images

--- src/components/Teammate.js
+++ src/components/Teammate.js
@@ -5,7 +5,7 @@ export default function Teammate(props){
     return(
         <div className="min-w-[300px] max-w-[300px] m-2 mx-auto py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-2 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
             <img
-                className="objectcover rounded-full h-[150px] w-[150px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+                className="objectcover rounded-full h-[150px] w-[150px] block mx-auto rounded-full sm:mx-0 sm:shrink-0"
                 src={props.img}
                 alt={props.name+"'s pfp"}/>
             <div className="text-center space-y-2 sm:text-left">
